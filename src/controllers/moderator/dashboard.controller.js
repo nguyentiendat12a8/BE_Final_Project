d158dbdb184ec0467d91e8aae54f56b7f889bb97
nguyentiendat12a8 <74580065+nguyentiendat12a8@users.noreply.github.com>
2022-04-20 18:11:16 +0700

    update dashboard l2

--- src/controllers/moderator/dashboard.controller.js
+++ src/controllers/moderator/dashboard.controller.js
@@ -9,47 +9,47 @@ exports.dashboard = async (req, res) => {
     const date = new Date(req.query.date)
     await Promise.all([Tour.find({ moderatorID: req.accountID }), HotelRoom.find({ moderatorID: req.accountID }), TourCustom.find({ moderatorID: req.accountID })])
         .then(async ([tour, room, tourCustom]) => {
-            var totalTour = 0
-            var totalTourCustom = 0
-            var totalRoom = 0
+            var totalPriceTour = 0
+            var totalPriceTourCustom = 0
+            var totalPriceRoom = 0
             async function getBillTour(i) {
                 var tour = await BillTour.find({ tourID: i._id })
                 tour.forEach(e => {
                     var dateBill = new Date(e.bookedDate)
                     if (date.getMonth() === dateBill.getMonth() && date.getFullYear() === dateBill.getFullYear()) {
-                        totalTour += e.price
+                        totalPriceTour += e.price
                     }
                 })
-                return totalTour
+                return totalPriceTour
             }
             async function getBillTourCustom(j) {
                 var tourCustom = await BillTour.find({ tourCustomID: j._id })
                 tourCustom.forEach(e => {
                     var dateBill = new Date(e.bookedDate)
                     if (date.getMonth() === dateBill.getMonth() && date.getFullYear() === dateBill.getFullYear()) {
-                        totalTourCustom += e.price
+                        totalPriceTourCustom += e.price
                     }
                 })
-                return totalTourCustom
+                return totalPriceTourCustom
             }
             async function getBillRoom(k) {
                 var room = await BillHotelRoom.find({ hotelRoomID: k._id })
                 room.forEach(e => {
                     var dateBill = new Date(e.createdAt)
                     if (date.getMonth() === dateBill.getMonth() && date.getFullYear() === dateBill.getFullYear()) {
-                        totalRoom += e.price
+                        totalPriceRoom += e.price
                     }
                 })
-                return totalRoom
+                return totalPriceRoom
             }
             await Promise.all(tour.map(i => getBillTour(i)))
             await Promise.all(tourCustom.map(j => getBillTourCustom(j)))
             await Promise.all(room.map(k => getBillRoom(k)))
             return res.status(200).send({
                 errorCode: 0,
-                totalTour,
-                totalTourCustom,
-                totalRoom,
+                totalPriceTour,
+                totalPriceTourCustom,
+                totalPriceRoom,
                 numberOfTour: tour.length,
                 numberOfHotelRoom: room.length,
                 numberOfTourCustom: tourCustom.length
